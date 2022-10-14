638063f9f0a501ae05b9046a4acc0aa9c86642e0
George Ceapa <georgec252a@yahoo.com>
2022-10-14 14:45:17 +0300

    first commit

--- target/generated-sources/kogito/org/acme/travels/Rules2E460E98FBDF296CF50748F35F942A65.java
+++ target/generated-sources/kogito/org/acme/travels/Rules2E460E98FBDF296CF50748F35F942A65.java
@@ -35,7 +35,7 @@ public class Rules2E460E98FBDF296CF50748F35F942A65 implements org.drools.model.M
 
     /**
      * With the following expression ID:
-     * org.drools.model.codegen.execmodel.generator.DRLIdGenerator@122df3b8
+     * org.drools.model.codegen.execmodel.generator.DRLIdGenerator@e005fe5
      */
     @Override
     public java.util.List<org.drools.model.Rule> getRules() {
